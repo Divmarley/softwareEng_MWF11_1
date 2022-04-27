0b6e41dab4fc46adc9cbdc658b7d7913499c3385
Divmarley <nexted.marley@gmail.com>
2022-04-27 11:46:57 +0000

    first commit

---
+++ classwork/profile.py
@@ -0,0 +1,56 @@
+ 
+
+
+"""
+variables 
+data types
+    string
+    numbers
+    bool
+    list 
+    tuples 
+    dict
+    sets
+
+casting
+if..else
+while loop 
+for loop
+ 
+---------slow down-------
+function
+    lambda
+
+arrarys
+
+classes and objects
+    inheritance
+
+------end slow ------
+iterators
+scope
+modules 
+    dates
+    Maths
+    json 
+pip
+try..except
+user input
+string formatting
+
+file handling 
+    CRUD ->file 
+
+python sql
+    database crud
+    tables crud
+
+
+monogoDB
+
+
+
+
+
+
+"""
