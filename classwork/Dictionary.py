68bfd6be0f6a662faa9baeaf8c6b57fb60f0ad9c
Divmarley <nexted.marley@gmail.com>
2022-05-13 12:13:16 +0000

    13/05/22

--- classwork/Dictionary.py
+++ classwork/Dictionary.py
@@ -33,17 +33,79 @@ person = {
 # print(len(person))
 # print(person)
 # "address": {"house":374,"location":"accra"}
-profile = {
-    "name":"kofi",
-    "email":"kofi@mail.com",
-    "gender":"male",
-    "phone": 2335544335,
-} 
+# profile = {
+#   "name":"kofi",
+#   "email":"kofi@mail.com",
+#   "gender":"male",
+#   "phone": 2335544335,
+# } 
 # print(profile.popitem()) 
 # print(profile.popitem()) 
 # profile.clear() 
-print(profile) 
+# print(profile) 
 
-newProfile =profile
+# newProfile =profile
 
-print(newProfile)
+# print(newProfile)
+
+
+# start Nested Dictionaries
+
+# myfamily = {
+#   "firstMember" :{
+#     'children':{
+#       'firstChild':"kofi",
+#       'secChild':"ama",
+#     }
+#   }, 
+# }
+
+# print(myfamily["firstMember"]['children']['secChild'])
+
+
+
+grandmum = {
+  'father':{
+    "name":"daddy",
+    "children":{
+      "firstchild":"ama",
+      "secchild":"kofi",
+    }
+  },
+  "fatherBother":{
+    "name":"big joe",
+    "children":{
+      "firstchild":"abena",
+      "secchild":"loveway",
+    }
+  },
+}
+
+print(grandmum["fatherBother"]["name"])
+print(grandmum["fatherBother"]["children"]["secchild"])
+# end Nested Dictionaries
+
+
+# methods 
+
+"""
+Method	             Description
+clear()	             Removes all the elements from the dictionary
+copy()	             Returns a copy of the dictionary
+fromkeys()	          Returns a dictionary with the specified keys and value
+get()	                Returns the value of the specified key
+items()	              Returns a list containing a tuple for each key value pair
+keys()	              Returns a list containing the dictionary's keys
+pop()	                Removes the element with the specified key
+popitem()	            Removes the last inserted key-value pair
+setdefault()	        Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	            Updates the dictionary with the specified key-value pairs
+values()	            Returns a list of all the values in the dictionary
+
+
+
+
+
+
+
+"""
