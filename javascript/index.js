9413d5e1ab37599b7389a35040ef4ed51bf4e458
Divmarley <nexted.marley@gmail.com>
2022-09-09 14:43:29 +0000

    variables and operator

--- javascript/index.js
+++ javascript/index.js
@@ -16,9 +16,90 @@ textContent="marley"  <div id="demo">marley</div>
 // document.getElementById('demo').textContent ='hey marley'
 /*jbjhkh */
 
-'ug834y83y047384'
-"ug834y83y047384"
-30
-true
+// 'ug834y83y047384'
+// "ug834y83y047384"
+// 30
+// true
 
-console.log(typeof('true'));
+// console.log(typeof('true'));
+
+// keyword variableName = datavalue;
+// let variableName = datavalue;
+// const variableName = datavalue;
+
+// variableName = datavalue;
+// let personName = "marley";
+// console.log(personName);
+
+// const personDoB = 1955;
+
+// school = "popl"
+// console.log(hisName);
+// console.log(hisName);
+
+// decleared variables
+// let personName , personAge;
+
+// assign variables
+// personName="marley";
+// personAge=100;
+// console.log(personAge);
+
+
+
+
+// start Operator
+    // arithmethic operators
+    // +  -addition
+    // /  division
+    // *  -mult
+    // -  -sub
+
+    let a ,b ;
+    a = 20;
+    b =40;
+
+//    console.log( a + b);
+//    console.log( a - b);
+//    console.log( a * b);
+//    console.log( a / b);
+//    console.log( a ** b);
+//    console.log( a++);
+//    console.log( a++);
+//    console.log( a++);
+//    console.log( a --);
+//    console.log( a --);
+//    console.log( a --);
+
+
+
+
+
+    // Assignment operators
+
+    /*Assignment operators assign values to JavaScript variables.
+
+        Operator	           Example	               Same As
+        =	                   x = y	                 x = y
+        +=	                   x += y	                 x = x + y
+        -=	                   x -= y	                 x = x - y
+        *=	                   x *= y	                 x = x * y
+        /=	                   x /= y	                 x = x / y
+        %=	                   x %= y	                 x = x % y
+        **=	                   x **= y	                 x = x ** y
+
+
+
+    */
+
+        console.log(a+=b); 
+        
+        // a= a + b;
+        // console.log(a);
+
+
+
+    // Comparison operators
+    // logical operators
+
+// end Operator
