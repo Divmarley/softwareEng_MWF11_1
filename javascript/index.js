3247980e375f24307427f80fb54dc8bf94f1a587
Divmarley <nexted.marley@gmail.com>
2022-09-09 12:47:35 +0000

    js

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+
+/*
+console.log()
+document.write()
+window.alert()
+innerHtml="marley"  <div id="demo">marley</div>
+textContent="marley"  <div id="demo">marley</div>
+
+*/
+
+// console.log("funny worgihdfgjhdjfgld");
+// document.write("funny worgihdfgjhdjfgld");
+// window.alert("funny him")
+
+// document.getElementById('demo').innerHTML ='hey marley'
+// document.getElementById('demo').textContent ='hey marley'
+/*jbjhkh */
+
+'ug834y83y047384'
+"ug834y83y047384"
+30
+true
+
+console.log(typeof('true'));
