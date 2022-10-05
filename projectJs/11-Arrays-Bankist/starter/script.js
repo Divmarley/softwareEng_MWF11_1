8e52f0113549105bb4f8f171e73755260a289871
Divmarley <nexted.marley@gmail.com>
2022-10-05 13:12:08 +0000

    event

--- projectJs/11-Arrays-Bankist/starter/script.js
+++ projectJs/11-Arrays-Bankist/starter/script.js
@@ -6,16 +6,14 @@
 
 // Data
 
-
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
-  pin: 1111, 
-  username :'js'
- 
+  pin: 1111,
+  username: 'js',
 };
- 
+
 const account2 = {
   owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
@@ -38,51 +36,6 @@ const account4 = {
 };
 
 const accounts = [account1, account2, account3, account4];
-
-function generate_username(allaccount){
-  allaccount.forEach(function(account){
-    account.username = account.owner.toLowerCase().split(' ').map((val)=>val[0]).join('');
-  });
- 
-}
- 
-generate_username(accounts);
-
-// console.log(accounts)
- 
-
-
-
-
-
-
-
-
-
-// const mygenUsername =account1.owner.toLowerCase().split(' ').map((val)=>val[0]).join('');
-// account1.username =mygenUsername;
-// console.log(account1);
-
-
-// console.log(account1);
-
-// account1.email ="@mainkdfnxdfckm.com"
-// // account1.owner ="sika kofi"
-
-// // account1.owner
-// const usernameArray =[];
-// const trying =account1["owner"].split(' ')
-// for (let i = 0; i < trying.length; i++) {
- 
-//   usernameArray.push(trying[i][0].toLowerCase())
-// }
-
-// // account1.push(usernameArray)
-// let usernameuser =usernameArray.join('')
-// account1.username=usernameuser
-// console.log(account1);
-// console.log();
- 
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -108,72 +61,85 @@ const inputTransferTo = document.querySelector('.form__input--to');
 const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin'); 
- 
+const inputClosePin = document.querySelector('.form__input--pin');
 
+function generate_username(allaccount) {
+  allaccount.forEach(function (account) {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(val => val[0])
+      .join('');
+  });
+}
+generate_username(accounts);
 
-// let theName =account1["owner"].split(' ')[0];
-// titleWelcome.innerHTML =`Welcome back, ${theName}`; //good
-// titleWelcome.innerHTML ="hello, " + name; //bad
-
-// btnLogin.addEventListener('click',function(e){
-//   e.preventDefault();
-//   console.log("you made a click");
-// });
 let currentAccount;
-btnLogin.addEventListener('click',(e)=>{
+btnLogin.addEventListener('click', e => {
   e.preventDefault();
   // console.log(inputLoginUsername.value);
-  console.log(typeof(Number(inputLoginPin.value)));
+  // console.log(typeof(Number(inputLoginPin.value)));
 
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value
   );
   // console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)){
-    labelWelcome.innerHTML =`welcome back,  ${currentAccount.owner.split(' ')[0].split(' ')[0]}`
-    containerApp.style.opacity =100;
-    inputLoginUsername.value=inputLoginPin.value='';
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    labelWelcome.innerHTML = `welcome back,  ${
+      currentAccount.owner.split(' ')[0].split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+    inputLoginUsername.value = inputLoginPin.value = '';
 
-  //  console.log( currentAccount.movements);
-    const mov =currentAccount.movements
-    
-    mov.forEach(function (mov, i) {
-      const type = mov > 0 ? 'deposit' : 'withdrawal';
-  
-      const html = `
-        <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-          <div class="movements__value">${mov}€</div>
-        </div>`;
-  
-      containerMovements.insertAdjacentHTML('afterbegin', html);
-    }); 
-    const mainBalance = currentAccount.movements.reduce((int,val)=>int+val)
-    console.log(mainBalance);
+    //  console.log( currentAccount.movements);
+    const mov = currentAccount.movements;
+    DisplayMovements(mov);
 
-    labelBalance.innerHTML =`${mainBalance}$`
-  
+    const mainBalance = mov.reduce((int, val) => int + val);
+
+    labelBalance.innerHTML = `${mainBalance}$`;
+
+    // display summary of withdrawal and deposit
+    calcDisplaySummary(currentAccount);
   }
-
-
-
 });
 
+const DisplayMovements = function (mov) {
+  containerMovements.innerHTML = '';
+  mov.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const calcDisplaySummary = function (acc) {
+  const deposit = currentAccount.movements
+    .filter(mov => mov > 0)
+    .reduce((previousValue, currentValue) => previousValue + currentValue);
+
+  labelSumIn.innerHTML = deposit;
+
+  const withdrawal = currentAccount.movements
+    .filter(mov => mov < 0)
+    .reduce((previousValue, currentValue) => previousValue + currentValue);
+  labelSumOut.innerHTML = withdrawal;
+};
 
 
-// document.getElementById('login__btn');
 
-// document.getElementsByClassName('login__btn')
-// document.getElementsByTagName('button')
+// Transfer money
+btnTransfer.addEventListener('click',(e)=>{
+  e.preventDefault();
+  console.log('qwertu');
 
-
-// document.querySelector('#login__btn')
-// document.querySelector('.login__btn')
-// document.querySelector('button')
-
-
-// console.log(10 === '10');
-
-
+})
